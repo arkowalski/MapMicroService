4be8a8b27f490d40004544d1896f06e3cae27156
arkowalski <arkadiusz.kowalski@digital.hmrc.gov.uk>
2017-10-17 16:00:08 +0100

    all changes working for 532 repos

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,7 +17,7 @@ object Main extends App {
     "\"/home/arkadiusz/Applications/hmrc-development-environment/hmrc/\"")
 
   //val filePath2 = scala.io.StdIn.readLine()
-//  val routesFiles = getFileTree(new File(filePath)).filter(_.getName.endsWith("app.routes"))
+  //  val routesFiles = getFileTree(new File(filePath)).filter(_.getName.endsWith("app.routes"))
   var serviceNameSet = Set[String]()
   println("you have entered: " + filePath)
   println("Loading data")
@@ -36,19 +36,19 @@ object Main extends App {
   }
 
   def startSearch(): List[Repo] = {
-      val repos = new File(filePath).list()
+    val repos = new File(filePath).list()
 
     val filtered = repos.filterNot(_.startsWith("."))
-    val data = for(i <- filtered) yield{
+    val data = for (i <- filtered) yield {
       println(i)
-             Repo(i, servicesThatCanTalkToMe(i, filtered.toList), servicesThatICanTalkTo(i))
-      }
-      data.toList
+      Repo(i, servicesThatCanTalkToMe(i, filtered.toList), servicesThatICanTalkTo(i))
+    }
+    data.toList
   }
 
   def servicesThatCanTalkToMe(me: String, repos: List[String]): List[String] = {
-    var services : List[String] = List()
-    for(i <- repos){
+    var services: List[String] = List()
+    for (i <- repos) {
       val servicesThatRepoTalksTo = getServicesThatRepoTalksTo(filePath + "/" + i)
       if (servicesThatRepoTalksTo.contains(me)) {
         services = i :: services
@@ -63,36 +63,36 @@ object Main extends App {
   }
 
 
-    def getServicesThatRepoTalksTo(path: String): List[String] = {
-//      val myConfigFile = new File(path + "/conf/application.conf")
-//      val config = ConfigFactory.parseFile(myConfigFile)
-      if(! new File(path + "/conf/application.conf").exists()){
-        List("could not find application.conf")
-      }
-      else {
-        val myConfigFileContents = scala.io.Source.fromFile(path + "/conf/application.conf").mkString
-        val config = ConfigFactory.parseString(myConfigFileContents).resolve(ConfigResolveOptions.defaults().setAllowUnresolved(true))
-
-        val prodMicroserviceServices = if (config.hasPath("Prod.microservice.services")) getProdMicroServiceServices(config) else ""
-        val microserviceServices = if (config.hasPath("microservice.services")) getMicroserviceServices(config) else ""
-        val combined = (prodMicroserviceServices + microserviceServices).replaceAll("[\\]\\[,\"]", " ").split(" ").filterNot(e => e == "")
-        combined.toList
-      }
+  def getServicesThatRepoTalksTo(path: String): List[String] = {
+    //      val myConfigFile = new File(path + "/conf/application.conf")
+    //      val config = ConfigFactory.parseFile(myConfigFile)
+    if (!new File(path + "/conf/application.conf").exists()) {
+      List("could not find application.conf")
     }
+    else {
+      val myConfigFileContents = scala.io.Source.fromFile(path + "/conf/application.conf").mkString
+      val config = ConfigFactory.parseString(myConfigFileContents).resolve(ConfigResolveOptions.defaults().setAllowUnresolved(true))
 
-    def getProdMicroServiceServices(config: Config): String = {
-      val prodMicroSerevices = new JSONObject(config.getConfig("Prod.microservice.services").toString
-        .replaceAll("play.\"\\$\\{appName\\}\".", "replaced").drop(26).dropRight(2)).names()
-      val successOrNot = if (prodMicroSerevices != null) prodMicroSerevices.toString else ""
-      successOrNot
+      val prodMicroserviceServices = if (config.hasPath("Prod.microservice.services")) getProdMicroServiceServices(config) else ""
+      val microserviceServices = if (config.hasPath("microservice.services")) getMicroserviceServices(config) else ""
+      val combined = (prodMicroserviceServices + microserviceServices).replaceAll("[\\]\\[,\"]", " ").split(" ").filterNot(e => e == "")
+      combined.toList
     }
+  }
 
-    def getMicroserviceServices(config: Config): String = {
-      val microserviceServices = new JSONObject(config.getObject("microservice.services").toString.replaceAll
-      ("play.\"\\$\\{appName\\}\".", "replaced").drop(19).dropRight(1)).names()
-      val succesOrNot = if (microserviceServices != null) microserviceServices.toString else ""
-      succesOrNot
-    }
+  def getProdMicroServiceServices(config: Config): String = {
+    // println(config.getObject("Prod.microservice.services").unwrapped().keySet())
 
+    val prodMicroSerevices = new JSONObject(config.getConfig("Prod.microservice.services").toString
+      .replaceAll("play.\"\\$\\{appName\\}\".", "replaced").drop(26).dropRight(2)).names()
+    val successOrNot = if (prodMicroSerevices != null) prodMicroSerevices.toString else ""
+    successOrNot
+  }
 
+  def getMicroserviceServices(config: Config): String = {
+    val microserviceServices = new JSONObject(config.getObject("microservice.services").toString.replaceAll
+    ("play.\"\\$\\{appName\\}\".", "replaced").drop(19).dropRight(1)).names()
+    val succesOrNot = if (microserviceServices != null) microserviceServices.toString else ""
+    succesOrNot
+  }
 }
